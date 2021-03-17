3c4702a75cdd207db135403d3cac7b2f32032bb2
enes.yenidogan <enes.yenidogan@useinsider.com>
2021-03-17 20:04:14 +0300

    update

--- page/login_page.py
+++ page/login_page.py
@@ -6,11 +6,12 @@ from base.page_base import BaseClass
 class DecathlonsgLogin:
     """Website login page to user logged in."""
 
-    email = 'jugo@getnada.com'
-    password = 'test1234'
-    EMAIL = (By.NAME, 'email')
-    PASSWORD = (By.NAME, 'password')
-    NEXT_BUTTON = (By.ID, "next-btn")
+    email = 'borehy@getnada.com'
+    password = 'Test1234*'
+    EMAIL = (By.ID, 'input-email-or-mobile')
+    PASSWORD = (By.ID, 'input-password')
+    NEXT_BUTTON = (By.CLASS_NAME, "sb-button.blue")
+    CONFRIM_BTN = (By.ID, "signin-button")
 
     def __init__(self, driver):
         self.driver = driver
@@ -22,7 +23,9 @@ class DecathlonsgLogin:
 
         """
         sleep(2)
+        self.methods.wait_for_element(self.EMAIL).click()
         self.methods.wait_for_element(self.EMAIL).send_keys(self.email)
         self.methods.wait_for_element(self.NEXT_BUTTON).click()
+        self.methods.wait_for_element(self.PASSWORD).click()
         self.methods.wait_for_element(self.PASSWORD).send_keys(self.password)
-        self.methods.wait_for_element(self.NEXT_BUTTON).click()
+        self.methods.wait_for_element(self.CONFRIM_BTN).click()
